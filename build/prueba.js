a69a7265678e014af94923315f93c34f03d3c9cf
Ander Arruza Fuertes <ander.arruza@gmail.com>
2022-03-07 08:37:48 +0100

    Typing of variables
    Getting the knowlage of how does the varibales work on TypeScript

--- build/prueba.js
+++ build/prueba.js
@@ -8,4 +8,19 @@ let nombre = 'Ander';
 let apellido = 'Arruza';
 //Concat an Array
 let nombre_apellido = `${nombre} ${apellido}`;
+//is not like in java 8, the System.out.println() is now console.log(variable)
 console.log(nombre_apellido);
+//Normal Array of String
+let nombres = ['Ander', 'Gonzalo'];
+console.log(nombres[1]);
+//2 Dimensional Arrays (in spanish Tuplas)
+let sitio = ['Erandio', 1672];
+//Enumerations
+var Estado;
+(function (Estado) {
+    Estado[Estado["Offline"] = 0] = "Offline";
+    Estado[Estado["Indefinido"] = 1] = "Indefinido";
+    Estado[Estado["Online"] = 2] = "Online";
+})(Estado || (Estado = {}));
+//Variable that contains an Enumeration
+let stat = Estado.Online;
--- prueba.ts
+++ prueba.ts
@@ -1,8 +1,8 @@
-//boolean variable
+//Boolean variable
 let isValid: boolean = true;
 
-//number varibale
-//is not like in java 8, the "int" is now "number"
+//Number varibale
+//Is not like in java 8, the "int" is now "number"
 let numero: number = 5;
 
 //string variable
@@ -11,8 +11,42 @@ let apellido: string = 'Arruza';
 
 //Concat an Array
 let nombre_apellido: string = `${nombre} ${apellido}`;
-//is not like in java 8, the System.out.println() is now console.log(variable)
+//Is not like in java 8, the System.out.println() is now console.log(variable)
 console.log(nombre_apellido);
 
+//Normal Array of String
+let nombres: string[] = ['Ander','Gonzalo'];
+//Print the second name
+console.log(nombres[1])
+
 //2 Dimensional Arrays (in spanish Tuplas)
-let sitio: [String,number] = ['Erandio',1672]
+let sitio: [string,number] = ['Erandio',1672]
+
+//Enumerations
+enum Estado{
+    Offline,Indefinido,Online
+}
+//Variable that contains an Enumeration
+let stat: Estado = Estado.Online;
+
+//In TypeScript there is a variable that has no type and it's called unknown
+let sinTipo: unknown = 'Hola';
+//This this of variable is usefull when you get information from a web-page
+//When you dont know what type are you going to recive
+sinTipo = 1;
+sinTipo = true;
+
+let nuevaCadena: string = 'Esto es una cadena';
+//You can't simply set an unknown variable to a non-unkown variable
+//nuevaCadena = sinTipo;
+
+//Variable any
+let tipoIndefinido: any = 'Hey';
+//any type variablets let you setting any type of varibale to them
+nuevaCadena = tipoIndefinido;
+
+//Functions
+function logger(): void{
+    console.log('Logger');
+
+}
