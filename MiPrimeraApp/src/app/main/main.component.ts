a627fa4562a0480bbf5db3b89f6eab7dbd97de9a
Ander Arruza Fuertes <ander.arruza@gmail.com>
2022-03-09 09:20:34 +0100

    Live cicle of a component
    - Explication of the  cicle of a component

--- MiPrimeraApp/src/app/main/main.component.ts
+++ MiPrimeraApp/src/app/main/main.component.ts
@@ -7,9 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
-  constructor() { }
+  constructor() {
+    console.log('***** Estoy en el constructor ****');
+    console.log('Lo uso para inicializar propiedades');
+   }
 
   ngOnInit(): void {
+    console.log('***** Estoy en el ngOnInit');
+    console.log('Lo uso para lanzar las acciones que arrancan el componente');
   }
-
+  ngAfterViewInit(){
+    console.log('***** Estoy en el ngAfterViewInit');
+    console.log('Lo uso para lanzar acciones iniciales dentro de la plantilla');
+  }
 }
