8609c21ebd1f06d6ac358967960558788233e782
Ander Arruza Fuertes <ander.arruza@gmail.com>
2022-03-09 12:37:48 +0100

    ngSwitch
    - ngSwitch exercise done.
    - Remember that if you need to pass a parameter that is a string, that value MUST be between "''". For example: *ngSwitchCase="'one'"

--- Directivas/src/app/app.module.ts
+++ Directivas/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SemaforoComponent } from './components/semaforo/semaforo.component';
 import { BuclesComponent } from './components/bucles/bucles.component';
+import { SwitchComponent } from './components/switch/switch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SemaforoComponent,
-    BuclesComponent
+    BuclesComponent,
+    SwitchComponent
   ],
   imports: [
     BrowserModule,
