e10e5b129a9c3cf8e4314f647cdd068150c56495
Ander Arruza Fuertes <ander.arruza@gmail.com>
2022-03-09 12:27:08 +0100

    better ngFor apparience
    - Upgrade on the exercise that iterate an array
    - Added objects in a table

--- Directivas/src/app/components/bucles/bucles.component.ts
+++ Directivas/src/app/components/bucles/bucles.component.ts
@@ -11,14 +11,19 @@ export class BuclesComponent implements OnInit {
 
   constructor() {
     this.personas = [
-      {nombre: 'Mario', apellidos: 'Pérez', edad: 36},
-      {nombre: 'Rosa', apellidos: 'García', edad: 20},
-      {nombre: 'Rocio', apellidos: 'Martín', edad: 18},
-      {nombre: 'Manuel', apellidos: 'Rodríguez', edad: 19},
+      { nombre: 'Mario', apellidos: 'Pérez', edad: 36 },
+      { nombre: 'Rosa', apellidos: 'García', edad: 20 },
+      { nombre: 'Rocio', apellidos: 'Martín', edad: 18 },
+      { nombre: 'Manuel', apellidos: 'Rodríguez', edad: 19 },
     ]
-   }
+  }
 
   ngOnInit(): void {
+  }
+  onClick() {
+    this.personas.push({
+      nombre: 'Ander', apellidos: 'Arruza', edad: '20'
+    })
   }
 
 }
