6e0293e1fb27a765ec51193703bbc44dabbb8c3c
Ander Arruza Fuertes <ander.arruza@gmail.com>
2022-03-11 10:59:02 +0100

    css changes Shopping app
    - added ngModel instead of using (input)

--- ListaDeLaCompra/src/app/componentes/formulario/formulario.component.ts
+++ ListaDeLaCompra/src/app/componentes/formulario/formulario.component.ts
@@ -11,8 +11,6 @@ export class FormularioComponent implements OnInit {
 
   producto: Producto;
   lista: Departamento[];
-  seleccionado: Departamento;
-  nombre: string;
 
   @Output() creado:EventEmitter<Producto>;
 
@@ -20,27 +18,14 @@ export class FormularioComponent implements OnInit {
     this.producto = new Producto('',0,Departamento.Carniceria);
     this.creado = new EventEmitter();
     this.lista = [Departamento.Carniceria,Departamento.Fruteria,Departamento.Hogar,Departamento.Panaderia];
-    this.seleccionado = Departamento.Carniceria;
-    this.nombre = '';
 
    }
 
   ngOnInit(): void {
   }
-  onInput($event: any){
-    this.producto.cantidad = $event.target.value;
-
-  }
-  onInputName($event: any){
-    this.producto.nombre = $event.target.value;
-  }
   onCreado(){
-    this.producto.departamento = this.seleccionado;
     this.creado.emit(this.producto);
     this.producto = new Producto('',0,Departamento.Carniceria);
   }
-  onChange($event:any){
-    this.producto.departamento = $event.target.value;
-
-  }
 }
+
