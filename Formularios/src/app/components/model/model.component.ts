65e9f733e0d2543f6a886c464d121e8eb7b4b942
Ander Arruza Fuertes <ander.arruza@gmail.com>
2022-03-15 13:59:00 +0100

    Creation of the project Forms
    - Created 2 modules (model, template)
    - Installed bootstrap css library
    - Simple Validations done (maxLength)
    - Customized Validations done (dni)

---
+++ Formularios/src/app/components/model/model.component.ts
@@ -0,0 +1,84 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-model',
+  templateUrl: './model.component.html',
+  styleUrls: ['./model.component.css']
+})
+export class ModelComponent implements OnInit {
+
+  formulario: FormGroup;
+
+  /**
+   * 
+   * valid (si todas las validaciones dan true) -- invalid
+   * pristinie (el campo no ha sido escrito) -- dirty (ya ha habido algo escrito aunque luego lo haya borrado)
+   * untouched (todavia no ha tenido el foco y lo habia perdido) -- touched (habia tenido el foco, pero lo ha perdido)
+   */
+
+  constructor() {
+    this.formulario = new FormGroup({
+      nombre: new FormControl('', [
+        Validators.required,
+        Validators.minLength(3)
+      ]),
+      apellidos: new FormControl('', [
+        Validators.maxLength(10)
+      ]),
+      edad: new FormControl('', [
+        this.edadValidator
+      ]),
+      dni: new FormControl('', [
+        this.dniValidator
+      ]),
+      password: new FormControl(),
+      repite_password: new FormControl(),
+      email: new FormControl('', [
+        Validators.pattern(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/)
+      ])
+    });
+  }
+
+  ngOnInit(): void {
+  }
+
+  onSubmit() {
+    console.log(this.formulario.value);
+  }
+
+  edadValidator(FormControl: any) {
+    const value = FormControl.value;
+    const max: number = 65;
+    const min: number = 18;
+    if (value >= 18 && value <= 65) {
+      console.log('A')
+      return null;
+    } else {
+      console.log('B')
+      return { edadValidator: { max, min } }
+    }
+
+  }
+  dniValidator(FormControl: any) {
+    const value: string = FormControl.value;
+    const letras: string = 'TRWAGMYFPDXBNJZSQVHLCKET';
+
+    if (/^\d{8}[a-zA-Z]$/.test(value)) {
+      const numeros: number = parseInt(value.substring(0, value.length - 1));
+      const letra: string = value.charAt(value.length - 1);
+
+      const calculo = numeros % 23;
+      const letraSeleccionada = letras.charAt(calculo);
+      if (letra.toUpperCase() == letraSeleccionada) {
+        return null;
+      } else {
+        return {dniValidator: 'La letra no coincide con el número'}
+      }
+    }else{
+      return {dniValidator: 'El dni está en formato erroneo'}
+    }
+
+
+  }
+}
