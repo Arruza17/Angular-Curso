8b9883c635b13663564325d11084baa5f3e75e17
Ander Arruza Fuertes <ander.arruza@gmail.com>
2022-03-17 12:50:26 +0100

    CRUD methods in the service done
    @Put, @Post, @Get & @Delete

--- HTTP/src/app/app.component.ts
+++ HTTP/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Post } from './model/post.model';
+import { PostsService } from './services/posts.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'HTTP';
+
+  title: string = 'a'
+  arrayPost: any[];
+  postFound: Post;
+  formulario: FormGroup;
+
+  constructor(private postService: PostsService) {
+    this.arrayPost = [];
+    this.postFound = new Post(0, 0, '', '');
+    this.formulario = new FormGroup({
+      title: new FormControl(''),
+      body: new FormControl(''),
+      userId: new FormControl('')
+    })
+  }
+
+  ngOnInit() {
+    this.postService.getAll()
+      .subscribe((post) => this.arrayPost = post)
+
+  }
+
+  onClick(postId: number) {
+    try {
+      this.postService.getById(postId)
+        .subscribe((post) => this.postFound = post)
+    } catch (error: any) {
+      console.log(error);
+    }
+
+  }
+  onPeticionPost() {
+    try {
+      const created = this.postService.create(new Post(
+        1, 0, 'Nuevo título', 'Este es el cuerpo del post'
+      )).subscribe(response => console.log(response));
+    } catch (error) {
+      console.log(error)
+    }
+
+  }
+  onSubmit() {
+    try {
+      this.postService.create(this.formulario.value)
+        .subscribe(response => console.log(response));
+    } catch (error) {
+      console.log(error);
+    }
+
+  }
+  onClickUpdate() {
+    this.postService.update(new Post(
+      3, 5, 'Edit', 'Body editado'
+    )).subscribe(responde => console.log(responde))
+  }
+  onClickDelete(){
+    this.postService.delete(1)
+    .subscribe(response => console.log(response))
+  }
 }
--- HTTP/src/app/services/posts.service.ts
+++ HTTP/src/app/services/posts.service.ts
@@ -1,5 +1,7 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { lastValueFrom, Observable } from 'rxjs';
+import { Post } from '../model/post.model';
 
 @Injectable({
   providedIn: 'root'
@@ -11,7 +13,32 @@ export class PostsService {
   constructor(private httpClient: HttpClient) {
     this.baseUrl = 'https://jsonplaceholder.typicode.com/posts';
   }
-  getAll(): void {
-    //return this.httpClient.get<any[]>(this.baseUrl).toPromise();
+  getAll(): Observable<Post[]> {
+    return this.httpClient.get<Post[]>(this.baseUrl);
+
+  }
+  getById(id: number): Observable<Post> {
+    return this.httpClient.get<Post>(this.baseUrl + "/" + id);
+  
+  }
+
+  create(post: Post): Observable<Post>{
+    const title = post.title;
+    const body = post.body;
+    const userId = post.userId;
+    const bodyRequest = { title,body,userId};
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json; charset=UTF-8'
+      })
+    }
+    return this.httpClient.post<Post>(this.baseUrl,bodyRequest,httpOptions);
+
+  }
+  update(post: Post):Observable<Post>{
+    return this.httpClient.put<Post>(this.baseUrl+"/"+post.id,post);
+  }
+  delete(id: number):Observable<any>{
+    return this.httpClient.delete<any>(this.baseUrl+"/"+id);
   }
 }
